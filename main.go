e1ca63cfb3063c52a73f35930987a762cb327d76
kwaw <17128203+amihere@users.noreply.github.com>
2024-05-18 02:00:22 +0000

    added view

--- main.go
+++ main.go
@@ -64,3 +64,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     // Note that we're not returning a command.
     return m, nil
 }
+
+func (m model) View() string {
+  s := "What should we buy at the market? \n\n"
+  for i, choice := range m.choices {
+    cursor := " "
+    if m.cursor == i {
+      cursor = ">"
+    }
+    checked := " "
+    if _, ok := m.selected[i]; ok {
+      checked = "x"
+    }
+    s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+  }
+  s += "\nPress q to quit.\n"
+
+  return s
+}
+
+
