094d6d972d9536547c5f2b7c567eb8500decf1ea
kwaw <17128203+amihere@users.noreply.github.com>
2024-05-18 02:07:18 +0000

    added main

--- main.go
+++ main.go
@@ -1,86 +1,92 @@
 package main
 
-import(
-  "fmt"
-  "os"
-  tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"os"
 )
 
 type model struct {
-  choices []string
-  cursor int
-  selected map[int]struct{}
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
 }
 
 func initialModel() model {
-  return model{
-    choices: []string{"Buy Carrots", "Buy celery", "Buy kakroo"},
-    selected: make(map[int]struct{}),
-  }
+	return model{
+		choices:  []string{"Buy Carrots", "Buy celery", "Buy kakroo"},
+		selected: make(map[int]struct{}),
+	}
 }
 
 func (m model) Init() tea.Cmd {
-  return nil
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    switch msg := msg.(type) {
+	switch msg := msg.(type) {
 
-    // Is it a key press?
-    case tea.KeyMsg:
+	// Is it a key press?
+	case tea.KeyMsg:
 
-        // Cool, what was the actual key pressed?
-        switch msg.String() {
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
 
-        // These keys should exit the program.
-        case "ctrl+c", "q":
-            return m, tea.Quit
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
 
-        // The "up" and "k" keys move the cursor up
-        case "up", "k":
-            if m.cursor > 0 {
-                m.cursor--
-            }
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
 
-        // The "down" and "j" keys move the cursor down
-        case "down", "j":
-            if m.cursor < len(m.choices)-1 {
-                m.cursor++
-            }
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
 
-        // The "enter" key and the spacebar (a literal space) toggle
-        // the selected state for the item that the cursor is pointing at.
-        case "enter", " ":
-            _, ok := m.selected[m.cursor]
-            if ok {
-                delete(m.selected, m.cursor)
-            } else {
-                m.selected[m.cursor] = struct{}{}
-            }
-        }
-    }
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
 
-    // Return the updated model to the Bubble Tea runtime for processing.
-    // Note that we're not returning a command.
-    return m, nil
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
 }
 
 func (m model) View() string {
-  s := "What should we buy at the market? \n\n"
-  for i, choice := range m.choices {
-    cursor := " "
-    if m.cursor == i {
-      cursor = ">"
-    }
-    checked := " "
-    if _, ok := m.selected[i]; ok {
-      checked = "x"
-    }
-    s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-  }
-  s += "\nPress q to quit.\n"
+	s := "What should we buy at the market? \n\n"
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+	s += "\nPress q to quit.\n"
 
-  return s
+	return s
 }
 
-
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Big error time %v", err)
+		os.Exit(1)
+	}
+}
